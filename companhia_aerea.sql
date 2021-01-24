d4afbd95ca6f9f74d011225ce3e56cfdb02a3908
manuferreira <rmanussa@outlook.com>
2021-01-23 21:10:18 -0300

    Script SQL sobre Companhia Aerea adicionado

---
+++ companhia_aerea.sql
@@ -0,0 +1,95 @@
+CREATE SCHEMA companhia_aerea;
+USE companhia_aerea;
+
+CREATE TABLE aeroporto (
+	Codigo_aeroporto VARCHAR(5) NOT NULL,
+    Nome VARCHAR(45) NOT NULL,
+    Cidade VARCHAR(45) NOT NULL,
+    Estado VARCHAR(45),
+    PRIMARY KEY(Codigo_aeroporto)
+);
+
+CREATE TABLE voo (
+	Numero_voo VARCHAR(6) NOT NULL,
+    Companhia_aerea VARCHAR(45) NOT NULL,
+    Dia_da_semana VARCHAR(45) NOT NULL,
+    PRIMARY KEY(Numero_voo)
+);
+
+CREATE TABLE trecho_voo(
+	Numero_trecho INT NOT NULL,
+    Numero_voo VARCHAR(6) NOT NULL,
+    Codigo_aeroporto_partida VARCHAR(5) NOT NULL,
+    Codigo_aeroporto_chegada VARCHAR(5) NOT NULL,
+    Horario_partida_previsto VARCHAR(10),
+    Horario_chegada_previsto VARCHAR(10),
+    PRIMARY KEY(Numero_trecho, Numero_voo)
+);
+
+CREATE TABLE instancia_trecho(
+	Numero_voo VARCHAR(6) NOT NULL,
+    Numero_trecho INT NOT NULL,
+    Data DATE NOT NULL,
+    Numero_assentos_disponiveis VARCHAR(45),
+    Codigo_aeronave VARCHAR(5),
+    Codigo_aeroporto_partida VARCHAR(5),
+    Horario_partida VARCHAR(10),
+    Codigo_aeroporto_chegada VARCHAR(5),
+    Horario_chegada VARCHAR(10),
+    PRIMARY KEY(Numero_voo, Numero_trecho, Data)
+);
+
+CREATE TABLE tarifa(
+	Numero_voo VARCHAR(6) NOT NULL,
+    Codigo_tarifa INT NOT NULL,
+    Quantidade INT,
+    Restricoes VARCHAR(50),
+    PRIMARY KEY(Numero_voo, Codigo_tarifa)
+);
+
+CREATE TABLE tipo_aeronave(
+	Nome_tipo_aeronave VARCHAR(20) NOT NULL,
+    Qtd_max_assentos VARCHAR(10),
+    Companhia VARCHAR(45),
+    PRIMARY KEY(Nome_tipo_aeronave)
+);
+
+CREATE TABLE pode_pousar(
+	Nome_tipo_aeronave VARCHAR(45) NOT NULL,
+    Codigo_aeroporto VARCHAR(5) NOT NULL,
+    PRIMARY KEY(Nome_tipo_aeronave, Codigo_aeroporto)
+);
+
+CREATE TABLE aeronave(
+	Codigo_aeronave VARCHAR(5) NOT NULL,
+    Numero_total_assentos VARCHAR(10),
+    Tipo_aeronave VARCHAR(20),
+    PRIMARY KEY(Codigo_aeronave)
+);
+
+CREATE TABLE reserva_assento(
+	Numero_voo VARCHAR(6) NOT NULL,
+    Numero_trecho INT NOT NULL,
+    Data DATE NOT NULL,
+    Numero_assento VARCHAR(10) NOT NULL,
+    Nome_cliente VARCHAR(50),
+    Telefone_cliente VARCHAR(10),
+    PRIMARY KEY(Numero_voo, Numero_trecho, Data, Numero_assento)
+);
+
+ALTER TABLE trecho_voo ADD FOREIGN KEY(Numero_voo) REFERENCES voo(Numero_voo);
+ALTER TABLE trecho_voo ADD FOREIGN KEY(Codigo_aeroporto_partida) REFERENCES aeroporto(Codigo_aeroporto);
+ALTER TABLE trecho_voo ADD FOREIGN KEY(Codigo_aeroporto_chegada) REFERENCES aeroporto(Codigo_aeroporto);
+ALTER TABLE instancia_trecho ADD FOREIGN KEY(Numero_voo) REFERENCES voo(Numero_voo);
+ALTER TABLE instancia_trecho ADD FOREIGN KEY(Numero_trecho) REFERENCES trecho_voo(Numero_trecho);
+ALTER TABLE instancia_trecho ADD FOREIGN KEY(Codigo_aeronave) REFERENCES aeronave(Codigo_aeronave);
+ALTER TABLE instancia_trecho ADD FOREIGN KEY(Codigo_aeroporto_partida) REFERENCES aeroporto(Codigo_aeroporto);
+ALTER TABLE instancia_trecho ADD FOREIGN KEY(Codigo_aeroporto_chegada) REFERENCES aeroporto(Codigo_aeroporto);
+ALTER TABLE tarifa ADD FOREIGN KEY(Numero_voo) REFERENCES voo(Numero_voo);
+ALTER TABLE pode_pousar ADD FOREIGN KEY(Nome_tipo_aeronave) REFERENCES tipo_aeronave(Nome_tipo_aeronave);
+ALTER TABLE pode_pousar ADD FOREIGN KEY(Codigo_aeroporto) REFERENCES aeroporto(Codigo_aeroporto);
+ALTER TABLE aeronave ADD FOREIGN KEY(Tipo_aeronave) REFERENCES tipo_aeronave(Nome_tipo_aeronave);
+ALTER TABLE reserva_assento ADD FOREIGN KEY(Numero_voo, Numero_trecho, Data) REFERENCES instancia_trecho(Numero_voo, Numero_trecho, Data);
+
+
+
