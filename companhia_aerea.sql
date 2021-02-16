354c8b7beb8db30691ad286969395eb148ef902c
manuferreira <rmanussa@outlook.com>
2021-02-16 14:56:50 -0300

    feat: tabelas populadas

--- companhia_aerea.sql
+++ companhia_aerea.sql
@@ -77,6 +77,64 @@ CREATE TABLE reserva_assento(
     PRIMARY KEY(Numero_voo, Numero_trecho, Data, Numero_assento)
 );
 
+
+/*INSERINDO NAS TABELAS*/
+INSERT INTO aeroporto VALUES 
+    ('10001','AEROPORTO DE MANUSSA','HELLCIFE','PE'),
+    ('30003','AEROPORTO DE RENATO', 'ORLINDA','MG'),
+    ('15030', 'AEROPORTO DE VICTOR', 'CAMARABRIGE','TI'),
+    ('20002','AEROPORTO DE NINGUEM','NOONEKNOWS','IDK');
+
+INSERT INTO voo VALUES
+    ('111116','MANUSENDS','QUARTA'),
+    ('222226','RENATRAVEL','SEGUNDA'),
+    ('333336','VITORRIVE','SEXTA'),
+    ('444446','NOONEGOES','DOMINGO');
+
+INSERT INTO trecho_voo VALUES
+    ('111116',1,'10001','20002','11:11','00:00'),
+    ('222226',2,'20002','15030','22:22','00:00'),
+    ('333336',3,'15030','30003','00:33','00:00'),
+    ('444446',4,'30003','10001','00:44','00:00');
+
+INSERT INTO instancia_trecho VALUES
+    ('111116',1,'2021/01/01','20','11115','10001','11:11','20002','00:00'),
+    ('222226',2,'2021/10/01','10','22225','20002','22:22','15030','00:00'),
+    ('333336',3,'2021/02/09','3','33335','15030','00:33','30003','00:00'),
+    ('444446',4,'2021/02/04','0','44445','30003','00:44','10001','00:00');
+
+INSERT INTO  tarifa VALUES
+    ('111116',11,20,'NA'),
+    ('222226',12,10,'NA'),
+    ('333336',13,3,'NA'),
+    ('444446',14,0,'NA');
+
+INSERT INTO tipo_aeronave VALUES
+    ('Jatinho','30','MANUSENDS'),
+    ('Boing','120','RENATRAVEL'),
+    ('Foguete','4','VITORRIVE'),
+    ('Teleporte','1000','NOONEGOES');
+
+INSERT INTO pode_pousar VALUES 
+    ('Jatinho','20002'),
+    ('Boing','15030'),
+    ('Foguete','30003'),
+    ('Teleporte','10001');
+
+INSERT INTO aeronave VALUES
+    ('11115','30','Jatinho'),
+    ('22225','120','Boing'),
+    ('33335','3','Foguete'),
+    ('44445','1000','Teleporte');
+
+
+INSERT INTO reserva_assento VALUES 
+    ('111116',1,'2021/01/01','M01','Manussa','8190909090'),
+    ('222226',2,'2021/10/01','R02','Renato','8198080808'),
+    ('333336',3,'2021/02/09','J03','J. Victor','8197070707'),
+    ('444446',4,'2021/02/04','N04','No One','8196060606');
+
+
 ALTER TABLE trecho_voo ADD FOREIGN KEY(Numero_voo) REFERENCES voo(Numero_voo);
 ALTER TABLE trecho_voo ADD FOREIGN KEY(Codigo_aeroporto_partida) REFERENCES aeroporto(Codigo_aeroporto);
 ALTER TABLE trecho_voo ADD FOREIGN KEY(Codigo_aeroporto_chegada) REFERENCES aeroporto(Codigo_aeroporto);
