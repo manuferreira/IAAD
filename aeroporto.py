2567958448db5addc742359a789b686c40bc75e0
João Victor <joaoovictor.21@gmail.com>
2021-02-22 12:12:52 -0300

    Criação dos botões separados

--- aeroporto.py
+++ aeroporto.py
@@ -1,18 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
 from db import *
-from trecho import tabela_trecho
-from instancia import tabela_instancia_trecho
-from aeronave import tabela_aeronave
-from voo import tabela_voo
-from reserva_assento import tabela_reserva_assento
-from aeroporto import tabela_aeroporto
-mydb = Database()
-
-root = Tk() #janela principal
-root.title('Companhia Aérea')
-root.geometry("700x350")
-
 
 # TABELA AEROPORTO
 def tabela_aeroporto():
@@ -152,29 +140,3 @@ def tabela_aeroporto():
     lista_aeroporto.bind('<<ListboxSelect>>', selecionar_item)
 
     populate_list()
-
-#Botões pra tela principal
-aeroporto = Button(root, text="Dados aeroporto", command=tabela_aeroporto)
-aeroporto.grid(row=20, column=0, padx=10)
-
-voo = Button(root, text="Dados voo", command=tabela_voo)
-voo.grid(row=20, column=1, padx=10)
-
-trecho = Button(root, text="Dados dos Trechos", command=tabela_trecho)
-trecho.grid(row=20, column=2, padx=10)
-
-instancia_trecho = Button(root, text="Dados das Instancias dos Trechos", command=tabela_instancia_trecho)
-instancia_trecho.grid(row=20, column=3, padx=10)
-
-aeronave = Button(root, text="Dados das Aeronaves", command=tabela_aeronave)
-aeronave.grid(row=21, column=0, padx=10, pady=10)
-
-reserva_assento = Button(root, text="Dados das Reserva dos Assentos", command=tabela_reserva_assento)
-reserva_assento.grid(row=21, column=1, padx=10, pady=10)
-
-root.mainloop()
-mydb.encerrar_conexao()
-
-
-
-
